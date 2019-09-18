ba20fb2995775c6188d9f3be422d130689f7a4de
Charles Roussel <c.roussel@criteo.com>
2019-09-18 17:18:49 +0200

    fix travis 2

--- ci/travis/before-install.sh
+++ ci/travis/before-install.sh
@@ -6,7 +6,8 @@ set -x
 eval "${MATRIX_EVAL}"
 ulimit -c unlimited -S
 
-if [[ "${TRAVIS_OS_NAME}" == "linux"]]; then
+if [ "${TRAVIS_OS_NAME}" == "linux"]; then
     ccache --show-stats
 fi
+docker pull $OS
 
