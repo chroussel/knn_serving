4a0c52a8f6a162abfe0d7db820d313ed534fe901
Charles Roussel <c.roussel@criteo.com>
2019-09-19 11:59:10 +0200

    Create docker files for ubuntu volume

---
+++ Dockerfile
@@ -0,0 +1,24 @@
+FROM ubuntu:18.04
+
+RUN export DEBIAN_FRONTEND=noninteractive && \
+    apt-get update -y -q && \
+    apt-get install -y -q --no-install-recommends \
+    ca-certificates \
+    ccache \
+    g++ \
+    gcc \
+    git \
+    ninja-build \
+    pkg-config \
+    tzdata \
+    wget \
+    cmake \
+    openssl \
+    libssl-dev \
+    && apt-get clean \
+    && rm -rf /var/lib/apt/lists/*
+
+ENV CC=gcc \
+    GXX=g++
+
+CMD [ "/knn/ci/build_script_cpp.sh" ]
--- src/cpp/main.cc
+++ src/cpp/main.cc
@@ -13,30 +13,34 @@ namespace po = boost::program_options;
 using grpc::Server;
 using grpc::ServerAsyncResponseWriter;
 using grpc::ServerBuilder;
-using grpc::ServerContext;
 using grpc::ServerCompletionQueue;
+using grpc::ServerContext;
 using grpc::Status;
+using knn::api::Knn;
 using knn::api::KnnRequest;
 using knn::api::KnnResponse;
-using knn::api::Knn;
 using namespace boost::filesystem;
 
-class KnnController final : public Knn::Service {
+class KnnController final : public Knn::Service
+{
 
 public:
-    KnnController() : knnService_(Euclidean, 101, 50) {
-
+    KnnController() : knnService_(Euclidean, 101, 50)
+    {
     }
 
-    void load_embeddings(std::string embeddings_path) {
+    void load_embeddings(std::string embeddings_path)
+    {
         auto path = absolute(embeddings_path);
         recursive_directory_iterator itr(path);
-        for(;itr != recursive_directory_iterator(); ++itr)
+        for (; itr != recursive_directory_iterator(); ++itr)
         {
-            if (itr->path().extension() == ".bin") {
+            if (itr->path().extension() == ".bin")
+            {
                 std::cout << "Parsing " << itr->path().string() << std::endl;
                 ifstream embedding_file(itr->path(), std::ios::binary);
-                while(!embedding_file.eof()) {
+                while (!embedding_file.eof())
+                {
                     int64_t product_id;
                     int32_t partner_id;
                     int32_t embedding_length;
@@ -52,15 +56,17 @@ public:
         }
     }
 
-    void load_indices(std::string indices_path) {
+    void load_indices(std::string indices_path)
+    {
         auto path = absolute(indices_path);
         recursive_directory_iterator itr(path);
-        for(;itr != recursive_directory_iterator(); ++itr)
+        for (; itr != recursive_directory_iterator(); ++itr)
         {
-            if (itr->path().extension() == ".bin") {
+            if (itr->path().extension() == ".bin")
+            {
                 std::cout << "Parsing " << itr->path().string() << std::endl;
                 std::vector<std::string> results;
-                boost::split(results, itr->path().string(), [](char c){return c == '-';});
+                boost::split(results, itr->path().string(), [](char c) { return c == '-'; });
 
                 int32_t index_id = std::stoi(results[1]);
                 auto *index = knnService_.loadIndex(index_id, itr->path().string());
@@ -69,18 +75,20 @@ public:
         }
     }
 
-    Status Search(ServerContext* context, const KnnRequest* request, KnnResponse* response) override {
+    Status Search(ServerContext *context, const KnnRequest *request, KnnResponse *response) override
+    {
         auto requestSize = request->user_events().size();
         std::vector<int32_t> index_ids;
         index_ids.reserve(requestSize);
         std::vector<size_t> queryLabels;
         queryLabels.reserve(requestSize);
-        std::vector<int64_t > timestamps;
+        std::vector<int64_t> timestamps;
         timestamps.reserve(requestSize);
-        std::vector<int32_t > eventTypes;
+        std::vector<int32_t> eventTypes;
         eventTypes.reserve(requestSize);
 
-        for (auto &event: request->user_events()) {
+        for (auto &event : request->user_events())
+        {
             index_ids.push_back(event.partner_id());
             queryLabels.push_back(event.product_id());
             timestamps.push_back(event.timestamp());
@@ -92,7 +100,8 @@ public:
         std::cout << "Receiving request with partner " << request->index_id() << std::endl;
         auto result = knnService_.get_closest_items(userData, modelData, request->index_id(), request->result_count());
 
-        for(auto &r: result.items) {
+        for (auto &r : result.items)
+        {
             auto product = response->add_products();
             product->set_score(r.first);
             product->set_product_id(r.second);
@@ -104,18 +113,21 @@ private:
     KnnService knnService_;
 };
 
-class KnnServer final {
+class KnnServer final
+{
 public:
-    KnnServer() {
-
+    KnnServer()
+    {
     }
 
-    void init(std::string embeddings_path, std::string indices_path) {
+    void init(std::string embeddings_path, std::string indices_path)
+    {
         service_.load_embeddings(embeddings_path);
         service_.load_indices(indices_path);
     }
 
-    void run(int server_port) {
+    void run(int server_port)
+    {
         std::string server_address("0.0.0.0:" + std::to_string(server_port));
 
         ServerBuilder builder;
@@ -130,30 +142,30 @@ private:
     KnnController service_;
 };
 
-int main(int argc, char** argv) {
+int main(int argc, char **argv)
+{
     int server_port;
     po::options_description server_config_desc("Allowed options");
-    server_config_desc.add_options()
-            ("help", "")
-            ("p,port", po::value<int>(&server_port)->default_value(8888), "port to listen to")
-            ("embeddings_path", po::value<std::string>(), "path to embeddings to load")
-            ("indices_path", po::value<std::string>(), "path to indices to load");
+    server_config_desc.add_options()("help", "")("p,port", po::value<int>(&server_port)->default_value(8888), "port to listen to")("embeddings_path", po::value<std::string>(), "path to embeddings to load")("indices_path", po::value<std::string>(), "path to indices to load");
 
     po::variables_map vm;
     po::store(po::parse_command_line(argc, argv, server_config_desc), vm);
     po::notify(vm);
 
-    if (vm.count("help")) {
+    if (vm.count("help"))
+    {
         std::cout << server_config_desc << std::endl;
         return 1;
     }
 
-    if (vm.count("embeddings_path") == 0) {
+    if (vm.count("embeddings_path") == 0)
+    {
         std::cout << "embeddings_path is missing" << std::endl;
         return 1;
     }
 
-    if (vm.count("indices_path") == 0) {
+    if (vm.count("indices_path") == 0)
+    {
         std::cout << "indices_path is missing" << std::endl;
         return 1;
     }
